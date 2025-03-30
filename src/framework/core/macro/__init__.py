c1f7db490ee27e7a30f80e51b0d610352df1bff5
niart120 <38847256+niart120@users.noreply.github.com>
2025-03-30 21:08:06 +0900

    __init__内でのimportをしないように(循環参照の解消)

--- src/framework/core/macro/__init__.py
+++ src/framework/core/macro/__init__.py
@@ -1,13 +1,1 @@
-from .base import MacroBase
-from .command import Command
-from .executor import MacroExecutor
-from .constants import Button, Hat, LStick, RStick
-
-__all__ = [
-    "MacroBase",
-    "Command",
-    "Button",
-    "Hat",
-    "LStick",
-    "RStick",
-]
+__all__ = ["MacroBase", "Command", "Button", "Hat", "LStick", "RStick"]
